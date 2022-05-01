4b454874fc4cee64397378bbb23e375c36ba805c
farzadjahandar <asdf!@#$A1371>
2022-05-01 13:26:55 -0400

    Update

--- HashMap:Table/HashNode.py
+++
@@ -1,9 +0,0 @@
-class HashNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
-
-
-if __name__ == '__main__':
-    x = HashNode(10, 7)
